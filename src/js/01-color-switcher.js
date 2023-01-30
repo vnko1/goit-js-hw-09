ea1a1e3a9799e48c220cfbb218c6593f1cf0ccc0
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-30 13:28:01 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
-s;
+const body = document.body;
+const startBtn = body.querySelector('[data-start]');
+const closeBtn = body.querySelector('[data-stop]');
+
+let intervalId;
+closeBtn.disabled = 'disabled';
+body.style.backgroundColor = localStorage.getItem('color');
+
+startBtn.addEventListener('click', onStartClick);
+closeBtn.addEventListener('click', onStopClick);
+
+function onStartClick({ target }) {
+  intervalId = setInterval(
+    () => (body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  target.disabled = 'disabled';
+  closeBtn.removeAttribute('disabled');
+}
+
+function onStopClick({ target }) {
+  clearInterval(intervalId);
+  target.disabled = 'disabled';
+  startBtn.removeAttribute('disabled');
+  localStorage.setItem('color', body.style.backgroundColor);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function bgColorSet() {
+  body.style.backgroundColor = getRandomHexColor();
+}
