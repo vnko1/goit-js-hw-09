de7c09a225eb62cecdfbb558a401a9abbbfa6e76
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-31 00:31:40 +0200

    Update 02-timer.js

---
+++ src/js/02-timer.js
@@ -0,0 +1,72 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+let selectedTime = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    getTime.call(options, selectedDates[0]);
+  },
+};
+
+const inputEl = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysOutput = document.querySelector('[data-days]');
+const hoursOutput = document.querySelector('[data-hours]');
+const minutesOutput = document.querySelector('[data-minutes]');
+const secondsOutput = document.querySelector('[data-seconds]');
+
+flatpickr(inputEl, options);
+startBtn.addEventListener('click', onStartClick);
+
+function getTime(selectedDate) {
+  if (options.defaultDate > selectedDate) {
+    alert('Please choose a date in the future');
+    return;
+  }
+  selectedTime = selectedDate;
+}
+
+function onStartClick() {
+  setInterval(setTimer, 1000);
+}
+
+function setTimer() {
+  const currentTime = Date.now();
+  const deltaTime = selectedTime - currentTime;
+  outputTimerValue(convertMs(deltaTime));
+}
+
+function outputTimerValue({ days, hours, minutes, seconds }) {
+  daysOutput.textContent = days;
+  hoursOutput.textContent = hours;
+  minutesOutput.textContent = minutes;
+  secondsOutput.textContent = seconds;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
