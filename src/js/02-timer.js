8fc4a5def5f1fd78f646eabb71ab3aa418a0b957
Andrey Valenko <andreyvalenko@gmail.com>
2023-01-30 17:48:22 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,10 +13,27 @@ const options = {
 
 const inputEl = document.getElementById('datetime-picker');
 const btnEl = document.querySelector('[data-start]');
-let date;
+const daysOutput = document.querySelector('data-days');
+const hoursOutput = document.querySelector('data-hours');
+const minutesOutput = document.querySelector('data-minutes');
+const secondsOutput = document.querySelector('data-seconds');
+
 btnEl.disabled = 'disabled';
 
+let date;
+
+// document.addEventListener('click', onStartClick);
+
 flatpickr(inputEl, options);
+
+// function onStartClick() {
+//   setInterval(updateCounter, 1000);
+// }
+
+// function updateCounter() {
+//   daysOutput.textContent = convertMs(date).days;
+//   secondsOutput.textContent = convertMs(date).seconds;
+// }
 
 function getTime(selectedDate) {
   if (selectedDate.getTime() < this.defaultDate.getTime()) {
@@ -24,6 +41,25 @@ function getTime(selectedDate) {
     return;
   }
   date = selectedDate;
-  console.log(date);
+  console.log(date.getUTCMilliseconds());
   btnEl.removeAttribute('disabled');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
